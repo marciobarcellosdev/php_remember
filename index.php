1994b1eeebcc0a82ca5262db93a665b408f6c08f
Marcio Barcellos <marcbrcx@gmail.com>
2024-10-19 16:32:59 -0300

    19/10/2024 16:32

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+function exceptions_error_handler($severity, $message, $filename, $lineno) {
+    throw new ErrorException($message, 0, $severity, $filename, $lineno);
+}
+
+set_error_handler('exceptions_error_handler');
+?>
+
+
 <?php include 'php/max_file_size.php'; ?>
 <?php include 'php/check_registered_files.php'; ?>
 <?php 
