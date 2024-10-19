40dda69eaa7f246e87fb70a4ef85d224e6b20579
Marcio Barcellos <marcbrcx@gmail.com>
2024-10-19 16:19:58 -0300

    19/10/2024 16:19

---
+++ index-old1.php
@@ -0,0 +1,9 @@
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+<?php include 'php/max_file_size.php'; ?>
+<?php include 'php/check_registered_files.php'; ?>
