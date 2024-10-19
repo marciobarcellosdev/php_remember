f5b88e90a3153ed883e59a043162f0d97180dc60
Marcio Barcellos <marcbrcx@gmail.com>
2024-10-19 16:30:17 -0300

    19/10/2024 16:30

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
+<?php include 'php/max_file_size.php'; ?>
+<?php include 'php/check_registered_files.php'; ?>
+<?php 
+//echo $_SERVER['HTTP_HOST'];
+//echo '<pre>'; print_r($_SERVER); echo '</pre>';
+//print_r ($_SERVER["HTTP_X_REAL_IP"]);
+//echo '<pre>'; print_r($_SERVER['HTTP_X_REAL_IP']); echo '</pre>';
 
+// $ip = isset($_SERVER['HTTP_CLIENT_IP']) 
+//     ? $_SERVER['HTTP_CLIENT_IP'] 
+//     : (isset($_SERVER['HTTP_X_FORWARDED_FOR']) 
+//       ? $_SERVER['HTTP_X_FORWARDED_FOR'] 
+//       : $_SERVER['REMOTE_ADDR']);
+
+// echo 'O IP E: '.$ip;
+
+try {
+  $filesCounter = checkRegisteredFiles();
+} catch (Exception $e) {
+  echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+
+?>
 
 <html>
 <head>
